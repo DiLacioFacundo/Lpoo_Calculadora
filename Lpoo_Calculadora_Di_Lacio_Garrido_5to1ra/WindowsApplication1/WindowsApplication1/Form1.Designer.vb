fc04635716ebf9a4234d473ea68184bb272c6773
Facundo Di Lacio <81651644+DiLacioFacundo@users.noreply.github.com>
2022-10-17 08:21:44 -0300

    Add files via upload

---
+++ Lpoo_Calculadora_Di_Lacio_Garrido_5to1ra/WindowsApplication1/WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,274 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.T7 = New System.Windows.Forms.Button()
+        Me.T4 = New System.Windows.Forms.Button()
+        Me.T1 = New System.Windows.Forms.Button()
+        Me.T0 = New System.Windows.Forms.Button()
+        Me.T8 = New System.Windows.Forms.Button()
+        Me.T5 = New System.Windows.Forms.Button()
+        Me.T2 = New System.Windows.Forms.Button()
+        Me.TDecimal = New System.Windows.Forms.Button()
+        Me.T9 = New System.Windows.Forms.Button()
+        Me.T6 = New System.Windows.Forms.Button()
+        Me.T3 = New System.Windows.Forms.Button()
+        Me.TSuma = New System.Windows.Forms.Button()
+        Me.TDividir = New System.Windows.Forms.Button()
+        Me.TMultiplicar = New System.Windows.Forms.Button()
+        Me.TResta = New System.Windows.Forms.Button()
+        Me.TResultado = New System.Windows.Forms.Button()
+        Me.TBorrar = New System.Windows.Forms.Button()
+        Me.areaNum = New System.Windows.Forms.TextBox()
+        Me.CALCULADORA = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'T7
+        '
+        Me.T7.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.T7.Location = New System.Drawing.Point(12, 116)
+        Me.T7.Name = "T7"
+        Me.T7.Size = New System.Drawing.Size(55, 35)
+        Me.T7.TabIndex = 0
+        Me.T7.Text = "7"
+        Me.T7.UseVisualStyleBackColor = True
+        '
+        'T4
+        '
+        Me.T4.Location = New System.Drawing.Point(12, 157)
+        Me.T4.Name = "T4"
+        Me.T4.Size = New System.Drawing.Size(55, 35)
+        Me.T4.TabIndex = 1
+        Me.T4.Text = "4"
+        Me.T4.UseVisualStyleBackColor = True
+        '
+        'T1
+        '
+        Me.T1.Location = New System.Drawing.Point(12, 198)
+        Me.T1.Name = "T1"
+        Me.T1.Size = New System.Drawing.Size(55, 35)
+        Me.T1.TabIndex = 2
+        Me.T1.Text = "1"
+        Me.T1.UseVisualStyleBackColor = True
+        '
+        'T0
+        '
+        Me.T0.Location = New System.Drawing.Point(12, 239)
+        Me.T0.Name = "T0"
+        Me.T0.Size = New System.Drawing.Size(55, 41)
+        Me.T0.TabIndex = 3
+        Me.T0.Text = "0"
+        Me.T0.UseVisualStyleBackColor = True
+        '
+        'T8
+        '
+        Me.T8.Location = New System.Drawing.Point(106, 116)
+        Me.T8.Name = "T8"
+        Me.T8.Size = New System.Drawing.Size(50, 35)
+        Me.T8.TabIndex = 4
+        Me.T8.Text = "8"
+        Me.T8.UseVisualStyleBackColor = True
+        '
+        'T5
+        '
+        Me.T5.Location = New System.Drawing.Point(106, 157)
+        Me.T5.Name = "T5"
+        Me.T5.Size = New System.Drawing.Size(50, 35)
+        Me.T5.TabIndex = 5
+        Me.T5.Text = "5"
+        Me.T5.UseVisualStyleBackColor = True
+        '
+        'T2
+        '
+        Me.T2.Location = New System.Drawing.Point(106, 198)
+        Me.T2.Name = "T2"
+        Me.T2.Size = New System.Drawing.Size(50, 35)
+        Me.T2.TabIndex = 6
+        Me.T2.Text = "2"
+        Me.T2.UseVisualStyleBackColor = True
+        '
+        'TDecimal
+        '
+        Me.TDecimal.Location = New System.Drawing.Point(106, 239)
+        Me.TDecimal.Name = "TDecimal"
+        Me.TDecimal.Size = New System.Drawing.Size(50, 41)
+        Me.TDecimal.TabIndex = 7
+        Me.TDecimal.Text = "."
+        Me.TDecimal.UseVisualStyleBackColor = True
+        '
+        'T9
+        '
+        Me.T9.Location = New System.Drawing.Point(196, 116)
+        Me.T9.Name = "T9"
+        Me.T9.Size = New System.Drawing.Size(54, 35)
+        Me.T9.TabIndex = 8
+        Me.T9.Text = "9"
+        Me.T9.UseVisualStyleBackColor = True
+        '
+        'T6
+        '
+        Me.T6.Location = New System.Drawing.Point(196, 157)
+        Me.T6.Name = "T6"
+        Me.T6.Size = New System.Drawing.Size(54, 33)
+        Me.T6.TabIndex = 9
+        Me.T6.Text = "6"
+        Me.T6.UseVisualStyleBackColor = True
+        '
+        'T3
+        '
+        Me.T3.Location = New System.Drawing.Point(196, 196)
+        Me.T3.Name = "T3"
+        Me.T3.Size = New System.Drawing.Size(54, 37)
+        Me.T3.TabIndex = 10
+        Me.T3.Text = "3"
+        Me.T3.UseVisualStyleBackColor = True
+        '
+        'TSuma
+        '
+        Me.TSuma.Location = New System.Drawing.Point(196, 239)
+        Me.TSuma.Name = "TSuma"
+        Me.TSuma.Size = New System.Drawing.Size(54, 41)
+        Me.TSuma.TabIndex = 11
+        Me.TSuma.Text = "+"
+        Me.TSuma.UseVisualStyleBackColor = True
+        '
+        'TDividir
+        '
+        Me.TDividir.Location = New System.Drawing.Point(279, 116)
+        Me.TDividir.Name = "TDividir"
+        Me.TDividir.Size = New System.Drawing.Size(49, 35)
+        Me.TDividir.TabIndex = 12
+        Me.TDividir.Text = "/"
+        Me.TDividir.UseVisualStyleBackColor = True
+        '
+        'TMultiplicar
+        '
+        Me.TMultiplicar.Location = New System.Drawing.Point(279, 157)
+        Me.TMultiplicar.Name = "TMultiplicar"
+        Me.TMultiplicar.Size = New System.Drawing.Size(49, 33)
+        Me.TMultiplicar.TabIndex = 13
+        Me.TMultiplicar.Text = "X"
+        Me.TMultiplicar.UseVisualStyleBackColor = True
+        '
+        'TResta
+        '
+        Me.TResta.Location = New System.Drawing.Point(279, 198)
+        Me.TResta.Name = "TResta"
+        Me.TResta.Size = New System.Drawing.Size(49, 35)
+        Me.TResta.TabIndex = 14
+        Me.TResta.Text = "-"
+        Me.TResta.UseVisualStyleBackColor = True
+        '
+        'TResultado
+        '
+        Me.TResultado.Location = New System.Drawing.Point(279, 239)
+        Me.TResultado.Name = "TResultado"
+        Me.TResultado.Size = New System.Drawing.Size(49, 41)
+        Me.TResultado.TabIndex = 15
+        Me.TResultado.Text = "="
+        Me.TResultado.UseVisualStyleBackColor = True
+        '
+        'TBorrar
+        '
+        Me.TBorrar.Location = New System.Drawing.Point(12, 286)
+        Me.TBorrar.Name = "TBorrar"
+        Me.TBorrar.Size = New System.Drawing.Size(316, 41)
+        Me.TBorrar.TabIndex = 16
+        Me.TBorrar.Text = "BORRAR"
+        Me.TBorrar.UseVisualStyleBackColor = True
+        '
+        'areaNum
+        '
+        Me.areaNum.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+                    Or System.Windows.Forms.AnchorStyles.Left) _
+                    Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.areaNum.Location = New System.Drawing.Point(12, 63)
+        Me.areaNum.Name = "areaNum"
+        Me.areaNum.Size = New System.Drawing.Size(316, 20)
+        Me.areaNum.TabIndex = 17
+        '
+        'CALCULADORA
+        '
+        Me.CALCULADORA.AutoSize = True
+        Me.CALCULADORA.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.CALCULADORA.Font = New System.Drawing.Font("Ravie", 15.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CALCULADORA.ForeColor = System.Drawing.Color.Red
+        Me.CALCULADORA.Location = New System.Drawing.Point(68, 18)
+        Me.CALCULADORA.Name = "CALCULADORA"
+        Me.CALCULADORA.Size = New System.Drawing.Size(210, 27)
+        Me.CALCULADORA.TabIndex = 18
+        Me.CALCULADORA.Text = "CALCULADORA"
+        Me.CALCULADORA.TextAlign = System.Drawing.ContentAlignment.TopCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.DarkBlue
+        Me.ClientSize = New System.Drawing.Size(340, 339)
+        Me.Controls.Add(Me.CALCULADORA)
+        Me.Controls.Add(Me.areaNum)
+        Me.Controls.Add(Me.TBorrar)
+        Me.Controls.Add(Me.TResultado)
+        Me.Controls.Add(Me.TResta)
+        Me.Controls.Add(Me.TMultiplicar)
+        Me.Controls.Add(Me.TDividir)
+        Me.Controls.Add(Me.TSuma)
+        Me.Controls.Add(Me.T3)
+        Me.Controls.Add(Me.T6)
+        Me.Controls.Add(Me.T9)
+        Me.Controls.Add(Me.TDecimal)
+        Me.Controls.Add(Me.T2)
+        Me.Controls.Add(Me.T5)
+        Me.Controls.Add(Me.T8)
+        Me.Controls.Add(Me.T0)
+        Me.Controls.Add(Me.T1)
+        Me.Controls.Add(Me.T4)
+        Me.Controls.Add(Me.T7)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents T7 As System.Windows.Forms.Button
+    Friend WithEvents T4 As System.Windows.Forms.Button
+    Friend WithEvents T1 As System.Windows.Forms.Button
+    Friend WithEvents T0 As System.Windows.Forms.Button
+    Friend WithEvents T8 As System.Windows.Forms.Button
+    Friend WithEvents T5 As System.Windows.Forms.Button
+    Friend WithEvents T2 As System.Windows.Forms.Button
+    Friend WithEvents TDecimal As System.Windows.Forms.Button
+    Friend WithEvents T9 As System.Windows.Forms.Button
+    Friend WithEvents T6 As System.Windows.Forms.Button
+    Friend WithEvents T3 As System.Windows.Forms.Button
+    Friend WithEvents TSuma As System.Windows.Forms.Button
+    Friend WithEvents TDividir As System.Windows.Forms.Button
+    Friend WithEvents TMultiplicar As System.Windows.Forms.Button
+    Friend WithEvents TResta As System.Windows.Forms.Button
+    Friend WithEvents TResultado As System.Windows.Forms.Button
+    Friend WithEvents TBorrar As System.Windows.Forms.Button
+    Friend WithEvents areaNum As System.Windows.Forms.TextBox
+    Friend WithEvents CALCULADORA As System.Windows.Forms.Label
+
+End Class
---
+++ Lpoo_Calculadora_Di_Lacio_Garrido_5to1ra/WindowsApplication1/WindowsApplication1/Form1.vb
@@ -0,0 +1,102 @@
+﻿Public Class Form1
+
+    Dim num1, num2, numTotal As Double
+    Dim bandera As Integer
+
+    Private Sub T7_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles T7.Click
+        areaNum.Text = areaNum.Text & T7.Text
+    End Sub
+
+    Private Sub T8_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles T8.Click
+        areaNum.Text = areaNum.Text & T8.Text
+    End Sub
+
+    Private Sub T9_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles T9.Click
+        areaNum.Text = areaNum.Text & T9.Text
+    End Sub
+
+    Private Sub T4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles T4.Click
+        areaNum.Text = areaNum.Text & T4.Text
+    End Sub
+
+    Private Sub T5_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles T5.Click
+        areaNum.Text = areaNum.Text & T5.Text
+    End Sub
+
+    Private Sub T6_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles T6.Click
+        areaNum.Text = areaNum.Text & T6.Text
+    End Sub
+
+    Private Sub T1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles T1.Click
+        areaNum.Text = areaNum.Text & T1.Text
+    End Sub
+
+    Private Sub T2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles T2.Click
+        areaNum.Text = areaNum.Text & T2.Text
+    End Sub
+
+    Private Sub T3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles T3.Click
+        areaNum.Text = areaNum.Text & T3.Text
+    End Sub
+
+    Private Sub T0_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles T0.Click
+        areaNum.Text = areaNum.Text & T0.Text
+    End Sub
+
+    Private Sub TDividir_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TDividir.Click
+        num1 = Val(areaNum.Text)
+        bandera = 4
+        areaNum.Clear()
+    End Sub
+
+    Private Sub TMultiplicar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TMultiplicar.Click
+        num1 = Val(areaNum.Text)
+        bandera = 3
+        areaNum.Clear()
+    End Sub
+
+    Private Sub TResta_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TResta.Click
+        num1 = Val(areaNum.Text)
+        bandera = 2
+        areaNum.Clear()
+    End Sub
+
+    Private Sub TResultado_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TResultado.Click
+        If bandera = 1 Then
+            num2 = Val(areaNum.Text)
+            numTotal = num1 + num2
+            areaNum.Text = CStr(numTotal)
+            bandera = 0
+        ElseIf bandera = 2 Then
+            num2 = Val(areaNum.Text)
+            numTotal = num1 - num2
+            areaNum.Text = CStr(numTotal)
+            bandera = 0
+        ElseIf bandera = 3 Then
+            num2 = Val(areaNum.Text)
+            numTotal = num1 * num2
+            areaNum.Text = CStr(numTotal)
+            bandera = 0
+        ElseIf bandera = 4 Then
+            num2 = Val(areaNum.Text)
+            numTotal = num1 / num2
+            areaNum.Text = CStr(numTotal)
+            bandera = 0
+        End If
+    End Sub
+
+    Private Sub TSuma_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TSuma.Click
+
+        num1 = Val(areaNum.Text)
+        bandera = 1
+        areaNum.Clear()
+    End Sub
+
+    Private Sub TDecimal_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TDecimal.Click
+        areaNum.Text = areaNum.Text & TDecimal.Text
+    End Sub
+
+    Private Sub TBorrar_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles TBorrar.Click
+        areaNum.Clear()
+    End Sub
+End Class
